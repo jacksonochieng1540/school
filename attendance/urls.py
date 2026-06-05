baa48ff9b13bcb13203020a3d0d9981b3cb51247
Jack ayaga <jacksonochieng1540@gmail.com>
2026-06-05 09:49:31 +0300

    Add attendance marking URL pattern

--- attendance/urls.py
+++ attendance/urls.py
@@ -5,7 +5,7 @@ app_name = 'attendance'
 
 urlpatterns = [
     path('', views.attendance_list, name='list'),
-    path('mark/', views.mark_attendance, name='mark'),  # ADD THIS LINE
+    path('mark/', views.mark_attendance, name='mark'),  
     path('bulk/<int:class_room_id>/', views.bulk_mark_attendance, name='bulk_mark'),
     path('bulk/<int:class_room_id>/<int:subject_id>/', views.bulk_mark_attendance, name='bulk_mark_subject'),
     path('my/', views.my_attendance, name='my_attendance'),
